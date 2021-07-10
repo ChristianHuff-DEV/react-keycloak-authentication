7096fef8ae9657cb052a1af6be95d66bf4e10441
Christian <Christian.Huff.DEV@gmail.com>
2021-07-10 16:38:14 +0100

    Integrate the authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import AuthContextProvider from "./context/AuthContextProvider";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContextProvider";
 import LoadingSpinner from "./components/LoadingSpinner";
+import Home from "./components/Home"
 
 function App() {
-  return (
-    <AuthContextProvider>
-      <LoadingSpinner />
-    </AuthContextProvider>
-  );
+	const authContext = useContext(AuthContext)
+
+	// Show the loading spinner while the user is not authenticated
+	if (!authContext.isAuthenticated) {
+		return <LoadingSpinner />
+	}
+	// If the user is authenticated display the home component
+	else {
+		return <Home/>
+	}
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthContextProvider from "./context/AuthContextProvider";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<AuthContextProvider>
+			<App />
+		</AuthContextProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
