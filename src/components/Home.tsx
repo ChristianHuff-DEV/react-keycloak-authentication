f1ec981de467e5696d75d358ea1e9da70997a92d
ChristianHuff-DEV <Christian.Huff.DEV@gmail.com>
2021-07-10 14:36:59 +0000

    Style home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,19 @@
+
 const Home = () => {
-  return <p>Home</p>;
+  return (
+    <section className="text-gray-600 body-font relative">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Welcome
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Username
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
