92ed057293016fbbc6d7f481330569c53c4b3659
ChristianHuff-DEV <Christian.Huff.DEV@gmail.com>
2021-07-10 12:53:23 +0000

    Add home component and display it

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,20 +1,8 @@
+import Home from "./components/Home";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home/>
   );
 }
 
