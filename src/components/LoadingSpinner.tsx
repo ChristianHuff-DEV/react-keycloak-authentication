864c3b610d51309ee9988d0b7553be42dbf55aa5
ChristianHuff-DEV <Christian.Huff.DEV@gmail.com>
2021-07-10 14:14:10 +0000

    Add loading spinner component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+import styles from "./LoadingSpinner.module.css"
+
+const LoadingSpinner = () => {
+  return (
+    <div className={`${styles.lds}`}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
