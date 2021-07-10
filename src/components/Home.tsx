012894bf4f1b40acbe9b8ba511907e1bb75c6d7e
Christian <Christian.Huff.DEV@gmail.com>
2021-07-10 16:44:51 +0100

    Display the username of the authenticated user

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContextProvider";
 
 const Home = () => {
+	const authContext = useContext(AuthContext)
+
   return (
     <section className="text-gray-600 body-font relative">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-12">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            Welcome
+            Welcome {authContext.username}
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Username
-          </p>
         </div>
       </div>
     </section>
